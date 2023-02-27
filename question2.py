fe818eb03c0c51786a84460df3e016950c24238b
Abhijith Prakash <99633082+IndexPosition@users.noreply.github.com>
2023-02-27 15:47:05 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,8 +1,22 @@
 import itertools
 
+"""
+Basically the question means if the number is 3 so the possible number of outcome will be 8.
+BBB
+BBR
+BRB
+BRR 
+RBB
+RBR
+RRB
+RRR 
+But the number of sequeces where R is more or equal is only 4, i.e., BRR, RBR, RRB, RRR. So that is what I need as the output.
+So, to get that I used itertools for storing all the outcomes in a list and then removing the ones with more number of B, after that counting the number of elements in the new list.
+"""
+
 def boolean_table(x):
-    elements = ['R', 'B']
-    combinations = itertools.product(elements, repeat=x)
+    variables = ['R', 'B']
+    combinations = itertools.product(variables, repeat=x)
     return list(map(''.join, combinations))
 
 T = int(input())
@@ -11,15 +25,15 @@ for _ in range(T):
     N = int(input())
     X=[]
     L = boolean_table(N)
-    print(L)
     countR, countB = 0, 0
     for i in L:
         for j in i:
             if j=="R":
                 countR+=1
-            elif j=="B":
+            else:
                 countB+=1
         if countR>=countB:
             X.append(i)
-            countR, countB = 0, 0
+            countR = countB = 0
+        
     print(len(X))
