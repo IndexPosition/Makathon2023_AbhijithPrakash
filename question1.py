ce122743a0854809e8a199d56a9e7128b58d53a3
Abhijith Prakash <99633082+IndexPosition@users.noreply.github.com>
2023-02-27 14:34:30 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -10,7 +10,7 @@ Like wise when we take:
     We cut the paper in 2 pieces to get the 1x1 squares.
 Similarly:
     n , m as 5, 3
-    We cut the paper in 2 pieces to get the 1x1 squares.
+    We cut the paper in 6 pieces to get the 1x1 squares.
 
 So to find the number of cuts needed we have to subtract 1 from each of the numbers, and then add them. Because the shape getting cut 2 times to get 3 pices, and 4 times to get 5 pieces. THere the formula (n-1)+(m-1)
 """
